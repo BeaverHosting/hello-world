dd4c4e8010f600c12ff805406c94d1b76b86617e
Julien M <julien.maupetit@protonmail.com>
2018-03-04 10:49:57 +0100

    Updates Kubernetes and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM golang:1.9.2
 EXPOSE 8080
-ENV customername unknown
 WORKDIR /go/src/app
 COPY main.go .
-CMD go run main.go -customer=${customername}
+CMD go run main.go -customer=${MY_CUSTOMER_NAME} -pod=${MY_POD_NAME}
--- main.go
+++ main.go
@@ -12,15 +12,17 @@ type ServerStatus struct {
 	Name         string    `json:"app_name"`
 	CustomerName string    `json:"customer_name"`
 	DateStart    time.Time `json:"date_start"`
+	PodName      string    `json:"pod_name"`
 	Status       int       `json:"status"`
 }
 
 func main() {
 
 	customer := flag.String("customer", "unknown", "the name of the customer")
+	podName := flag.String("pod", "unknown", "the name of the pod")
 	flag.Parse()
 
-	app := ServerStatus{"hello-world", *customer, time.Now().UTC(), http.StatusOK}
+	app := ServerStatus{"hello-world", *customer, time.Now().UTC(), *podName, http.StatusOK}
 
 	data, _ := json.Marshal(app)
 
